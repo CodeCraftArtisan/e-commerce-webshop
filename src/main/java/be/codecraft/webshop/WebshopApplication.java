4a7f7cf2e87bdf3677e0e204d2db265dd88c925b
narciss98 <narciss98@gmail.com>
2025-02-11 19:24:37 +0100

    Added config to resolve cors issues

--- src/main/java/be/codecraft/webshop/WebshopApplication.java
+++ src/main/java/be/codecraft/webshop/WebshopApplication.java
@@ -2,8 +2,10 @@ package be.codecraft.webshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "be.codecraft.webshop")
 public class WebshopApplication {
 
 	public static void main(String[] args) {
