817cabb822acd07aed7557bde22eb9cb68a98416
narciss98 <narciss98@gmail.com>
2025-02-12 12:06:18 +0100

    Temp cors config

--- src/main/java/be/codecraft/webshop/controller/securityJWT/config/CustomCorsConfiguration.java
+++ src/main/java/be/codecraft/webshop/controller/securityJWT/config/CustomCorsConfiguration.java
@@ -13,7 +13,7 @@ public class CustomCorsConfiguration implements CorsConfigurationSource {
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://codecraft-webshop-project.netlify.app"));
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         return config;
